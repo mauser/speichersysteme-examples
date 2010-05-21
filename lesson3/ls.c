8bb72a59322e6c10d38e31bd3de6c8b3fda4322d
mauser <sebastian@sebastian-laptop.(none)>
2010-05-21 10:42:14 +0200

    added size and uid to ls.c

--- lesson3/ls.c
+++ lesson3/ls.c
@@ -2,6 +2,7 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
 struct dirent *readdir(DIR *dir);
 
@@ -17,8 +18,15 @@ int main(){
 			if( dp->d_type == 4 ){
 					printf("\E[31m %s %i \E[30m\n",dp->d_name, dp->d_type);
 			} else {
-    				printf("%s %i\n",dp->d_name, dp->d_type);
-			}
+				
+				struct stat s;
+	  			stat( dp->d_name , &s);
+          			
+    				
+				printf("%s %i %i %i \n",dp->d_name, dp->d_type , s.st_size, s.st_uid);
+	  			
+      			}
+			
 		} else {
         		closedir(dirp);
         		return -1;
